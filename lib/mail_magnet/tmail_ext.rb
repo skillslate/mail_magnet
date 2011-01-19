2e7bc7da7ce0bdbc1a7035cafd64cfaae60b7a92
Lawrence Wang <lawrence@levityisland.com>
2011-01-18 19:03:32 -0500

    add headers instead of munging mail body

--- lib/mail_magnet/tmail_ext.rb
+++ lib/mail_magnet/tmail_ext.rb
@@ -9,14 +9,9 @@ TMail::Mail.class_eval do
       :bcc => override(:bcc, recipients)
     }
 
-    parts = [ "--- Original recipients ---" ]
     [ :to, :cc, :bcc ].each do |target|
-      parts << target.to_s.capitalize + ": " + (original_addresses[target] ? original_addresses[target].join(', ') : '(none)')
+      self["X-Original-#{target.to_s.capitalize}"] = (original_addresses[target] ? original_addresses[target].join(', ') : '(none)')
     end
-    parts << "---------------------------"
-
-    line_break = self.content_type =~ /(html)/ ? "<br />" : "\n"
-    self.body = parts.join(line_break) + line_break*2 + self.body
   end
 
   def override(method, recipients)
